f1dde0cc7f9f34d26239021c9d802d10cfa94f72
tshm <tshm@andLinux>
2009-10-23 14:42:01 -0400

    add delete function

--- tv.rb
+++ tv.rb
@@ -7,6 +7,8 @@ begin
 	require 'ramaze/log/informer'
 end
 
+# recorded item entity.
+# use ruby internal hash to identify each instance.
 class Item
 	def self.list
 		Dir["public/video/*.flv"].map do |flvname|
@@ -16,12 +18,21 @@ class Item
 			h = {}
 			h[:title] = mpgname.sub(/.*\/.+_(.+)_.+\.mpg/,'\1')
 			h[:filename] = flvname.sub(/public/,'')
-			h[:date] = date_ch.sub(/^(..)(..)(..)/,'\1.\2.\3')
+			h[:date] = date_ch.sub(/^(..)(..)(..).*/,'\1.\2.\3')
 			h[:ch] = date_ch.sub(/\d+/,'')
 			h[:hour] = hour
 			h[:min] = min
 			h
 		end
+	end
+
+	def self.delete(pathname)
+		dum, dum, date_ch, ename, hour, min, ext = pathname.split(/[\/_.]/)
+		mpgname = Dir["public/video/#{date_ch}_*_#{hour}.#{min}.mpg"][0]
+		flvname = Dir["public#{pathname}"][0]
+		File.delete(mpgname)
+		File.delete(flvname)
+		[mpgname, flvname]
 	end
 end
 
@@ -36,5 +47,11 @@ class MainController < Ramaze::Controller
 		else
 			@items = Item.list if session[:loggedin]
 		end
+	end
+
+	def delete
+		a = Item.delete(request[:id])
+		flash[:message] = "Deleted: #{a[0]} & #{a[1]}"
+		redirect('/')
 	end
 end
