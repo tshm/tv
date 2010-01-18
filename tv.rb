2d8c9134bd9f8fac30ccc692786ffab11f000b07
Toshihide Shimayama <2sm@csc.jp>
2010-01-19 03:39:38 +0900

    add sort feature

--- tv.rb
+++ tv.rb
@@ -6,33 +6,39 @@ begin
 	#require "date"
 	require 'ramaze/log/informer'
 end
+TVDIR = "public/video"
 
 # recorded item entity.
 # use ruby internal hash to identify each instance.
 class Item
-	def self.list
-		Dir["public/video/*.flv"].map do |flvname|
-			dum, dum, date_ch, ename, hour, min, ext = flvname.split(/[\/_.]/)
-			mpgname = Dir["public/video/#{date_ch}_*_#{hour}.#{min}.mpg"][0]
-			next unless mpgname
-			h = {}
-			h[:mpgname] = mpgname.sub(/public\//,'')
-			h[:title] = mpgname.sub(/.*\/.+_(.+)_.+\.mpg/,'\1')
-			h[:filename] = flvname.sub(/public/,'')
-			h[:date] = date_ch.sub(/^(..)(..)(..).*/,'\1.\2.\3')
-			h[:ch] = date_ch.sub(/\d+/,'')
-			h[:hour] = hour
-			h[:min] = min
-			h
+	attr_reader :ch, :title, :mpgname, :flvname, :time
+
+	def initialize(flvname)
+		dum, dum, date_ch, ename, hour, min, ext = flvname.split(/[\/_.]/)
+		mpgname = Dir["#{TVDIR}/#{date_ch}_*_#{hour}.#{min}.mpg"][0]
+		return nil unless mpgname
+		h = {}
+		@mpgname = mpgname.sub(/public\//,'')
+		@title = mpgname.sub(/.*\/.+_(.+)_.+\.mpg/,'\1')
+		@flvname = flvname.sub(/public/,'')
+		@ch = date_ch.sub(/\d+/,'')
+		date_ch =~ /^(..)(..)(..).*/
+		@time = Time.local($1, $2, $3, hour, min)
+		return nil unless @time
+	end
+
+	def self.list(sort_key=nil)
+		items = Dir["#{TVDIR}/*.flv"].map {|flvname| Item.new(flvname)}.compact.reject {|i| nil == i.time}
+		if sort_key
+			items.sort_by {|i| i.__send__(sort_key)}
+		else
+			items
 		end
 	end
 
-	def self.delete(pathname)
-		dum, dum, date_ch, ename, hour, min, ext = pathname.split(/[\/_.]/)
-		mpgname = Dir["public/video/#{date_ch}_*_#{hour}.#{min}.mpg"][0]
-		flvname = Dir["public#{pathname}"][0]
-		File.delete(mpgname)
-		File.delete(flvname)
+	def delete
+		File.delete("public/" + @mpgname)
+		File.delete("public/" + @flvname)
 		[mpgname, flvname]
 	end
 end
@@ -55,11 +61,12 @@ class MainController < Ramaze::Controller
 
 	def index
 		@title = "tv"
-		@items = Item.list if session[:loggedin]
+		sort_key = request["sort"] ? request["sort"].to_sym : nil
+		@items = Item.list(sort_key) if session[:loggedin]
 	end
 
 	def delete
-		a = Item.delete(request[:pathname])
+		a = Item.new(request[:pathname]).delete
 		flash[:message] = "Deleted: #{a[0]} & #{a[1]}"
 		redirect_referer
 	end
