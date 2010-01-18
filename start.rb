acefb7778d860d6a03c308d6ad0ab6aa1af0ba68
Toshihide Shimayama <2sm@csc.jp>
2010-01-19 03:39:24 +0900

    do not spawn in debug mode

--- start.rb
+++ start.rb
@@ -5,19 +5,20 @@ $LOAD_PATH.unshift(__DIR__)  # add project home dir to ruby load_path
 CONF = YAML.load_file("conf.yml") || {}
 require 'tv'
 
+option = {:adapter => :mongrel, :root => '.'}
+option[:port] = CONF['port'] if CONF['port']
 unless ARGV.empty?
 	require "ruby-debug"
 	Ramaze.options.mode = :dev
 	Ramaze::Log.level = Logger::DEBUG
+	Ramaze.start option
 else #deploy mode
 	Ramaze.options.mode = :live
 	Ramaze::Log.level = Logger::INFO
 	Ramaze::Log.loggers << Ramaze::Logger::Informer.new("ramaze.log")
+	fork {
+		Process.setsid
+		Ramaze.start option
+	}
 end
 
-option = {:adapter => :mongrel, :root => '.'}
-option[:port] = CONF['port'] if CONF['port']
-fork {
-	Process.setsid
-	Ramaze.start option
-}
